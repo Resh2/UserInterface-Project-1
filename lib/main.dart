ed11e6828bbab129e3faaff2d9757885d95bdfbb
Reshma <mehreshma58@gmail.com>
2020-02-28 16:12:11 -0600

    Linear gradient color update

--- lib/main.dart
+++ lib/main.dart
@@ -108,8 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-        
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+              ),
     );
   }
 }
