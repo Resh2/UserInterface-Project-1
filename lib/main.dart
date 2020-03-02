1e6f274b8f4318072b961991125203ce6e7e8458
Reshma <mehreshma58@gmail.com>
2020-03-02 10:36:28 -0600

    added top bar plush,phonecase & tees

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,26 @@
 import 'package:flutter/material.dart';
-
-void main() => runApp(MyApp());
-
+ 
+void main() {
+  runApp(MyApp());
+}
+ 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-    //    // is not restarted.
-      //  primarySwatch: Colors.white,
-       
-      ),
-      home: MyHomePage(title: 'Baby Yoda Merchandise'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-    
-      _counter++;
-    });
-  }
-  
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-  appBar: AppBar(
-  title: Text("BABY YODA MERCHANDISE"), 
-  actions: <Widget>[
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(text: 'Plush' ),
+                Tab(text: 'Phone Case',),
+                Tab(text: 'Tees'),
+              ],
+            ),
+            title: Text('BABY YODA MERCHANDISE'),
+            actions: <Widget>[
     Padding(
       padding: EdgeInsets.only(right: 20.0),
       child: GestureDetector(
@@ -63,7 +32,7 @@ class _MyHomePageState extends State<MyHomePage> {
       )
     ),
   ],
-      flexibleSpace:  Container(
+       flexibleSpace:  Container(
       decoration: BoxDecoration(
       gradient: LinearGradient(
       begin: Alignment.topCenter,
@@ -73,42 +42,64 @@ class _MyHomePageState extends State<MyHomePage> {
   ),
 ),
 ),
-
-        body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+          
+          body: TabBarView(
+            children: [
+             PlushScreen(),
+             PhoneCaseScreen(),
+             TeesScreen()
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )
     );
+  }
+}
+ 
+class PlushScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body: Center(
+              child: 
+              Text('Display all the Plush', 
+                style: TextStyle(fontSize: 21),)
+              )
+            )
+          );
+  }
+}
+ 
+class PhoneCaseScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body: Center(
+              child: 
+              Text('Display all the Phone Case',
+                style: TextStyle(fontSize: 21),)
+              )
+            )
+          );
+  }
+}
+ 
+class TeesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body: Center(
+              child: 
+              Text('Display all the Tees',
+                style: TextStyle(fontSize: 21),)
+              )
+            )
+          );
   }
 }
