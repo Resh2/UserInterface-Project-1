d763ea524a6c1099f76657355f8ca362e16fdfcb
Reshma <mehreshma58@gmail.com>
2020-03-04 19:14:40 -0600

    app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,30 @@
 import 'package:flutter/material.dart';
- 
+
 void main() {
-  runApp(MyApp());
+  runApp(new MainPage());
 }
- 
-class MyApp extends StatelessWidget {
+
+class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: TabBar(
-              tabs: [
-                Tab(text: 'Plush' ),
-                Tab(text: 'Phone Case',),
-                Tab(text: 'Tees'),
-              ],
-            ),
-            title: Text('BABY YODA MERCHANDISE'),
-            actions: <Widget>[
-    Padding(
+        home: DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: [
+              Tab(text: 'Plush'),
+              Tab(
+                text: 'Phone Case',
+              ),
+              Tab(text: 'Tees'),
+            ],
+          ),
+          title: Text('BABY YODA MERCHANDISE'),
+          backgroundColor: Color(0xFF1B9772),
+          actions: <Widget>[
+             Padding(
       padding: EdgeInsets.only(right: 20.0),
       child: GestureDetector(
         onTap: () {},
@@ -32,74 +35,69 @@ class MyApp extends StatelessWidget {
       )
     ),
   ],
-       flexibleSpace:  Container(
-      decoration: BoxDecoration(
-      gradient: LinearGradient(
-      begin: Alignment.topCenter,
-      end: Alignment.bottomCenter,
-      colors: [const Color(0xFF1B9772), const Color(0xFF0D4837)], 
-    ),
-  ),
-),
-),
-          
-          body: TabBarView(
-            children: [
-             PlushScreen(),
-             PhoneCaseScreen(),
-             TeesScreen()
-            ],
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [const Color(0xFF1B9772), const Color(0xFF0D4837)],
+              ),
+            ),
           ),
         ),
-      )
-    );
+        body: TabBarView(
+          children: [PlushScreen(), PhoneCaseScreen(), TeesScreen()],
+        ),
+      ),
+    ));
   }
 }
- 
+
 class PlushScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-            body: Center(
-              child: 
-              Text('Display all the Plush', 
-                style: TextStyle(fontSize: 21),)
-              )
-            )
-          );
+          body: Center(
+            child: Text(
+              'Display all the Plush',
+              style: TextStyle(fontSize: 21),
+            ),
+          ),
+          backgroundColor: Colors.white,//Color(0xFF4F5B55),
+        ));
   }
 }
- 
+
 class PhoneCaseScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-            body: Center(
-              child: 
-              Text('Display all the Phone Case',
-                style: TextStyle(fontSize: 21),)
-              )
-            )
-          );
+          body: Center(
+              child: Text(
+            'Display all the Phone Case',
+            style: TextStyle(fontSize: 21),
+          )),
+          backgroundColor: Colors.white,//Color(0xFF4F5B55),
+        ));
   }
 }
- 
+
 class TeesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-            body: Center(
-              child: 
-              Text('Display all the Tees',
-                style: TextStyle(fontSize: 21),)
-              )
-            )
-          );
+          body: Center(
+              child: Text(
+            'Display all the Tees',
+            style: TextStyle(fontSize: 21),
+          )),
+          backgroundColor: Colors.white,//Color(0xFF4F5B55),
+        ));
   }
 }
